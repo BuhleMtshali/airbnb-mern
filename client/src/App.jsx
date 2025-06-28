b32311d236c89a9cb83c605d4e1b9fc236426e5d
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-28 15:58:27 +0200

    updated the app, index and the login jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import './index.css';
 import './app.css';
 import Index from './components';
+import Login from './components/Login';
 
 function App() {
   return(
     <Routes>
       <Route index element={<Index />} />
+      <Route path='/login' element={<Login />} />
     </Routes>
   )
 }
