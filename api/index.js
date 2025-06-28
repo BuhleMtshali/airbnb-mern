539db17b71e72fd8a3e3a3aa88177b1bb9d42538
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-28 19:06:13 +0200

    updated the index file

--- api/index.js
+++ api/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(express.json());
+app.use(cors({
+  credentials: true,
+  origin: 'http://localhost:5173',
+}));
 
 app.get('/test', (req, res) => {
     res.json('Test is ok');
 });
+
+app.post('/register', (req, res) => {
+  const { name, email, password } = req.body;
+  res.json({ name, email, password })
+})
 
 app.listen(4000, () => {
   console.log('🚀 Server running at http://localhost:4000');
