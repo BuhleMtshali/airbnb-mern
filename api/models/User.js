ad6f415f8f63f4bc3b8ac28eceb796d24367c85c
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-28 19:34:55 +0200

    Updated the backend

---
+++ api/models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    name: String,
+    email: { tyepe: String, unique: true },
+    password: String,
+});
+
+const userModel = mongoose.model('User', userSchema);
+module.exports = userModel;
