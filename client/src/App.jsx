7ab2dd9877c70e8b8c3ac8dc1b9d2741b0793d38
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-28 18:11:23 +0200

    updated the app and the register app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,17 @@ import './index.css';
 import './app.css';
 import Index from './components';
 import Login from './components/Login';
+import Layout from './Layout';
+import Register from './components/Register';
 
 function App() {
   return(
     <Routes>
-      <Route index element={<Index />} />
-      <Route path='/login' element={<Login />} />
+      <Route path='/' element={<Layout />} >
+        <Route index element={<Index />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Route>
     </Routes>
   )
 }
