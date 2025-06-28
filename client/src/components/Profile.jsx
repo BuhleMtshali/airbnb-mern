146acf8383d8163d04561504661a9826f3afe995
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-28 22:35:39 +0200

    updated the account and the profile

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,25 +1,32 @@
-import { use, useContext } from "react";
+import { use, useContext, useState } from "react";
 import { UserContext } from "../UserContext";
 import { Navigate, useParams } from "react-router-dom";
 import AccountNav from "../AccountNav";
+import axios from "axios";
 
 const Profile = () => {
+    const [redirect, setRedirect] = useState(null);
     const { ready, user, setUser } = useContext(UserContext);
     let { subpage } = useParams();
     if (subpage === undefined) subpage = 'profile';
-    if(!ready){
-        return 'Loading...';
+
+    async function logout(){
+        await axios.post('/logout');
+        setRedirect('/');
+        setUser(null);
     }
-    if (ready && !user){
-        return <Navigate to={'/login'} />
-    }
+
+    if(!ready) return 'Loading...';
+    if(ready && !user && !redirect) return <Navigate to={'/login'}/>
+    if(redirect) return <Navigate to={redirect}/>
+
     return(
         <div>
             <AccountNav />
             {subpage === 'profile' && (
                 <div className="text-center max-w-lg mx-auto">
                     Logged in as {user.name} ({user.email}) <br/>
-                    <button className="primary max-w-sm mt-2">Logout</button>
+                    <button onClick={logout} className="primary max-w-sm mt-2">Logout</button>
                 </div>
             )}
         </div>
