15760f9baecfdaf39b4740d7f84a3c32cefed8c9
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-28 20:45:37 +0200

    updated the login file

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-64">
                 <h1 className="text-4xl text-center mb-4">Login</h1>
-                <form className="max-w-md mx-auto">
+                <form className="max-w-md mx-auto" onSubmit={handleLoginSubmit}>
                     <input type="email" placeholder="your@email.com"/>
                     <input type="password" placeholder="password" />
                     <button className="primary">Login</button>
