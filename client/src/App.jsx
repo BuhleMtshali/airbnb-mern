8bf8e24abab1c9ed27234c3115fabd5c219e5270
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-28 15:08:02 +0200

    updated the tailwind configeration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
 
-import './App.css';
 import './index.css';
 
 function App() {
   return (
-    <div className="bg-red-200 min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">AirBnB MERN</h1>
+    <div className='bg-blue-200'>
+      Airbnb Clone
     </div>
   );
 }
