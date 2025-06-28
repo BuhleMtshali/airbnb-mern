e00666ac70985f6a15f8d36f9bc149daa1fb1f78
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-28 18:58:53 +0200

    updated the register component

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -21,10 +21,10 @@ const Register = () => {
        <div className="mt-4 grow flex items-center justify-around">
         <div className="mt-64">
             <h1 className="text-4xl text-center mb-4">Register</h1>
-            <form className="max-w-md mx-auto">
-                <input type="text" placeholder="John Doe" />
-                <input type="email" placeholder="your@email.com"/>
-                <input type="password" placeholder="password"/>
+            <form className="max-w-md mx-auto" onSubmit={registerUser}>
+                <input type="text" placeholder="John Doe" value={name} onChange={e => setName(e.target.value)}/>
+                <input type="email" placeholder="your@email.com" value={email} onChange={e => setEmail(e.target.value)}/>
+                <input type="password" placeholder="password" value={password} onChange={e => setPassword(e.target.value)}/>
                 <button className="primary">Register</button>
                 <div className="text-center py-2 text-gray-500">
                     Already a member?{" "}
