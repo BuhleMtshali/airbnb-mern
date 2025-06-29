1cd04038bf574658769d99f7f861df9430b48fe1
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-29 09:29:40 +0200

    updated the components

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,7 +1,7 @@
-
-const ImageComponent = ({ src, ...rest }) => {
-  const finalSrc = src && src.includes('https://') ? src : `http://localhost:4000/uploads/${src}`;
-  return <img {...rest} src={finalSrc} alt="" />;
-};
-
-export default ImageComponent;
+const Image = ({ src, ...rest }) => {
+    src = src && src.includes('https://') ? src : 'http://localhost:4000/uploads/' + src;
+    return (
+        <img {...rest} src={src} alt={''} />
+    );
+}
+export default Image;
