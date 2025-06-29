a8fbb2964d9d876801f96cd06708312101018a97
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-29 10:10:20 +0200

    updated the backend

--- api/models/Place.js
+++ api/models/Place.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const placeSchema = new mongoose.Schema({
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     title: String,
     address: String,
     photos: [String],
@@ -15,4 +15,5 @@ const placeSchema = new mongoose.Schema({
 });
 
 const PlaceModel = mongoose.model('Place', placeSchema);
+
 module.exports = PlaceModel;
--- api/models/User.js
+++ api/models/User.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true }, 
-  password: { type: String, required: true },
-  role: { type: String, enum: ['user', 'host', 'admin'], default: 'user' },
+const UserSchema = new Schema({
+    name: String,
+    email: { type: String, unique: true },
+    password: String,
 });
 
-module.exports = mongoose.model('User', UserSchema);
+const UserModel = mongoose.model('User', UserSchema);
+module.exports = UserModel;
