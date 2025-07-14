c80206ce4f0b6217dc4d73f35d34abe2bb9a813b
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-15 00:17:28 +0200

    updated the layout

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,25 +1,34 @@
-import React from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
 import { CategorySelector } from './components/CategorySelector';
 import { PropertyGrid } from './components/PropertyGrid';
 import { Footer } from './components/Footer';
+import React from 'react';
 
 const Layout = () => {
   const location = useLocation();
+  const pathname = location.pathname;
+
+  // Check if we're on /account or any of its subpages
+  const isAccountPage = pathname.startsWith('/account');
 
   return (
     <div className="py-4 px-8 flex flex-col min-h-screen">
       <Header />
-      <Hero />
-      <CategorySelector />
 
-      {/* Show PropertyGrid only on home */}
-      {location.pathname === '/' && <PropertyGrid />}
-
-      <Footer />
-      <Outlet />
+      {isAccountPage ? (
+        // Render account pages
+        <Outlet />
+      ) : (
+        // Render homepage
+        <>
+          <Hero />
+          <CategorySelector />
+          <PropertyGrid />
+          <Footer />
+        </>
+      )}
     </div>
   );
 };
